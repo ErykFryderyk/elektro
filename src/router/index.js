ca69de32439ae55b9b0af26156b2fc120128e8d4
ErykFryderyk <grzeniu098@gmail.com>
2024-05-22 23:04:14 +0200

    set routering viwes

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import ContactView from "../views/ContactView.vue";
+import AboutView from "../views/AboutView.vue";
+import ServicesView from "../views/ServicesView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
+    component: HomeView, // do jakiego komponentu będzie przenosić po kliknięciu
   },
   {
     path: "/about",
     name: "about",
+    component: AboutView, // do jakiego komponentu będzie przenosić po kliknięciu
+  },
+  {
+    path: "/services",
+    name: "services",
+    component: ServicesView, // do jakiego komponentu będzie przenosić po kliknięciu
+  },
+  {
+    path: "/contact",
+    name: "contact",
+    component: ContactView, // do jakiego komponentu będzie przenosić po kliknięciu
   },
 ];
 
