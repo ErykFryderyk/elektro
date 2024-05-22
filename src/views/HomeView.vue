e7142c59faa5e392256e3949f7725ade3ec63706
ErykFryderyk <grzeniu098@gmail.com>
2024-05-22 23:05:22 +0200

    created home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+<template>
+  <div class="home">
+    <div class="hero">
+      <img src="" alt="tunder-icon" />
+    </div>
+    <h2>Elektro</h2>
+    <p>O firmie</p>
+    <button>Sprawdź naszą ofertę</button>
+    <img src="" alt="logo-elektro" />
+    <h2>Czym się zajmujemy</h2>
+    <div class="">
+      <img src="" alt="" />
+      <h3>Instalacje</h3>
+      <ul>
+        <li>Instalacje elektryczne w domach i firmach</li>
+        <li>Instalacje elektryczne w domach i firmach</li>
+        <li>Instalacje elektryczne w domach i firmach</li>
+      </ul>
+    </div>
+    <h3>Nasze Realizacje</h3>
+    <!-- Slider -->
+    <h3>Skontaktuj się z nami</h3>
+    <p>
+      Jesteśmy do Twojej dyspozycji od poniedziałku do piątku w godzinach
+      8.00-16.00
+    </p>
+    <button>Skontaktuj się</button>
+    <!-- footer -->
+    <img src="" alt="logo-elektro" />
+  </div>
+</template>
