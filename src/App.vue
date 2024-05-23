ee21adf54de67893ecbf11c3e58c1cb296cfb989
ErykFryderyk <grzeniu098@gmail.com>
2024-05-23 23:09:56 +0200

    added conteiner style

--- src/App.vue
+++ src/App.vue
@@ -99,6 +99,23 @@ p {
 img {
   max-width: 100%;
 }
+.container {
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px;
+
+  @media (min-width: 768px) {
+    width: 750px;
+  }
+  @media (min-width: 992px) {
+    width: 970px;
+  }
+  @media (min-width: 1200px) {
+    width: 1170px;
+  }
+}
 .sticky {
   position: fixed;
   top: 0;
