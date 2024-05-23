184e4782b57ef5e7f4b27d47542335371760d777
ErykFryderyk <grzeniu098@gmail.com>
2024-05-23 23:01:55 +0200

    added feature show and hide mobile menu

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -1,5 +1,9 @@
 <script setup>
 import { ref } from "vue";
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
 
 const menuItems = ref([
   { string: "Strona główna", path: "/" },
@@ -7,51 +11,59 @@ const menuItems = ref([
   { string: "O nas", path: "/about" },
   { string: "Kontakt", path: "/contact" },
 ]);
+const isHamburgerClicked = computed(() => store.state.isHamburgerClicked);
 </script>
 
 <template>
-  <div class="mobile-menu">
-    <div class="mobile-menu">
-      <div class="mobile-menu__logo-wrapper">
-        <router-link to="/">
-          <span>Logo</span>
-        </router-link>
-      </div>
-      <nav class="mobile-nav">
-        <ul class="mobile-nav__items">
-          <router-link
-            v-for="(item, index) in menuItems"
-            :key="index"
-            @click="handleClick"
-            :to="item.path"
-          >
-            <li class="mobile_nav__item">
-              {{ item.string }}
-            </li>
-          </router-link>
-        </ul>
-      </nav>
+  <div class="mobile-menu" :class="{ active: !isHamburgerClicked }">
+    <div class="mobile-menu__logo-wrapper">
+      <router-link to="/">
+        <span>Logo</span>
+      </router-link>
     </div>
-    <div class="menu-overlay" @click="handleClick"></div>
+    <nav class="mobile-nav">
+      <ul class="mobile-nav__items">
+        <router-link
+          v-for="(item, index) in menuItems"
+          :key="index"
+          :to="item.path"
+        >
+          <li class="mobile_nav__item">
+            {{ item.string }}
+          </li>
+        </router-link>
+      </ul>
+    </nav>
   </div>
+  <div class="menu-overlay"></div>
 </template>
 
 <style lang="scss">
 .mobile-menu {
-  display: flex;
+  position: fixed;
+  top: 70px;
+  left: 0;
+  bottom: 0;
+  right: 0;
   flex-direction: column;
+  z-index: 990;
+  background: transparent;
+  backdrop-filter: blur(4.5px);
+  transition: transform 0.3s ease;
+}
+
+.active {
+  transform: translateX(100%);
+}
+
+.menu-overlay {
+  display: none;
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 98;
+  z-index: 97;
   width: 100vw;
   height: 100vh;
-  background: #dcdcdc41;
   backdrop-filter: blur(4.5px);
-  transition: transform 0.3s ease;
-
-  &--hide {
-    transform: translateX(-100%);
-  }
 }
 </style>
