7b8c703fd6511a00a3b9c9f4d8909ddc687dc2a2
ErykFryderyk <grzeniu098@gmail.com>
2024-05-27 23:58:27 +0200

    created template for button

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button>
+    <slot>Click Me!</slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+  display: inline-flex;
+  padding: 18px 20px;
+  justify-content: center;
+  align-items: center;
+  color: #002332;
+  font-family: Montserrat;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+}
+
+.bg-orange {
+  background: #fcbc53;
+}
+.bg-granat {
+  background-color: #002332;
+  color: #fff;
+}
+</style>
