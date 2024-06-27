30228289cf3533753332bb588517bc97c86bfdf2
ErykFryderyk <grzeniu098@gmail.com>
2024-06-27 22:29:52 +0200

    set corrert position on elements into nav-bar

--- src/components/DesktopNavigation.vue
+++ src/components/DesktopNavigation.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from "vue";
 import LogoElectro from "../assets/svg/LogoElectro.vue";
+import CustomButton from "./CustomButton.vue";
 
 const menuItems = ref([
   { string: "Strona główna", path: "/" },
@@ -11,16 +12,23 @@ const menuItems = ref([
 </script>
 <template>
   <div class="desktop-navigation">
-    <LogoElectro />
-    <ul>
-      <router-link
-        v-for="(item, index) in menuItems"
-        :key="index"
-        :to="item.path"
-      >
-        <li class="mobile_nav__item">{{ item.string }}</li>
+    <div class="container container--inline desktop-menu">
+      <router-link :to="'/'">
+        <LogoElectro style="width: 203px" />
       </router-link>
-    </ul>
+      <nav class="navigation-links">
+        <ul class="navigation-links__items">
+          <router-link
+            v-for="(item, index) in menuItems"
+            :key="index"
+            :to="item.path"
+          >
+            <li class="navigation-links__item">{{ item.string }}</li>
+          </router-link>
+        </ul>
+      </nav>
+      <CustomButton class="bg-orange">+48 513 587 427</CustomButton>
+    </div>
   </div>
 </template>
 
@@ -38,6 +46,31 @@ const menuItems = ref([
 
   @media (min-width: 1000px) {
     padding: 0 50px;
+  }
+}
+.desktop-menu {
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+}
+
+.navigation-links {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &__items {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+  }
+  &__item {
+    color: #f2f2f2;
+    font-family: Montserrat;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    text-transform: uppercase;
   }
 }
 </style>
