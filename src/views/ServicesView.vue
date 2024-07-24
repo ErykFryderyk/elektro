fa8aa7f6119c8f4b330bdfd1258170c3596bfb7e
ErykFryderyk <grzeniu098@gmail.com>
2024-07-24 23:25:09 +0200

    finished mobile view in services-page

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -1,5 +1,187 @@
+<script setup>
+// COMPONENTS
+import FooterElektro from "../components/FooterElektro.vue";
+import CustomButton from "@/components/CustomButton.vue";
+// ICON
+import SmallThunderIcon from "@/assets/svg/SmallThunderIcon.vue";
+import ClearDayIcon from "@/assets/svg/ClearDayIcon.vue";
+import HeatIcon from "@/assets/svg/HeatIcon.vue";
+import AvgPaceIcon from "@/assets/svg/AvgPaceIcon.vue";
+import InstantMixIcon from "@/assets/svg/InstantMixIcon.vue";
+</script>
 <template>
-  <div class="">
-    <h1>Usługi</h1>
+  <div class="services">
+    <div class="container">
+      <div class="">
+        <div class="">
+          <p style="font-size: 16px; color: #797979; margin-bottom: 24px">
+            Strona główna > Nasza oferta
+          </p>
+        </div>
+        <!-- eslint-disable-next-line -->
+        <h1 class="title title--h2 title--bold">Co oferujemy?</h1>
+        <div class="underline underline--mb"></div>
+        <div class="image-wrapper">
+          <div class="rectangle rectangle--yellow"></div>
+          <img src="../assets/img/services.jpeg" alt="services-hero-image" />
+        </div>
+
+        <h2 class="title title--h2 title--uppercase title-small">
+          Wszystko czego potrzebujesz!
+        </h2>
+        <h2
+          class="title title--h2 title--uppercase title-small title--gray title-mb"
+        >
+          Z zakresu elektryki
+        </h2>
+        <!-- eslint-disable-next-line -->
+        <p class="text">
+          Jesteśmy doświadczonymi elektrykami z wiedzą w zakresie różnorodnych
+          instalacji elektrycznych. Zapewniamy kompleksowe usługi elektryczne,
+          zgodne z najnowszymi standardami i przepisami.
+        </p>
+      </div>
+      <div class="">
+        <div class="services-icon">
+          <div class="services-icon__rect"></div>
+          <SmallThunderIcon class="transform" />
+        </div>
+        <h2 class="title title--h2 title--bold">Prace elektryczne</h2>
+        <!-- eslint-disable-next-line -->
+        <p class="text">
+          Dlaczego Wybrać Nasze Usługi: Doświadczeni elektrycy z wiedzą w
+          zakresie różnorodnych instalacji elektrycznych Specjalizacja w
+          instalacjach fotowoltaicznych, dostarczając energię ze źródeł
+          odnawialnych Kompleksowe podejście do montażu pomp ciepła, z
+          uwzględnieniem integracji z systemem grzewczym Skontaktuj się z Nami
+          Dziś: Zespół doświadczonych elektryków gotów do obsługi różnorodnych
+          projektów Zapewniamy kompleksowe usługi elektryczne, zgodne z
+          najnowszymi standardami i przepisami
+        </p>
+        <div class="services-icon">
+          <div class="services-icon__rect"></div>
+          <ClearDayIcon class="transform" />
+        </div>
+        <h2 class="title title--h2 title--bold">Fotowoltaika</h2>
+        <!-- eslint-disable-next-line -->
+        <p class="text">
+          Dlaczego Wybrać Nasze Usługi: Doświadczeni elektrycy z wiedzą w
+          zakresie różnorodnych instalacji elektrycznych Specjalizacja w
+          instalacjach fotowoltaicznych, dostarczając energię ze źródeł
+          odnawialnych Kompleksowe podejście do montażu pomp ciepła, z
+          uwzględnieniem integracji z systemem grzewczym Skontaktuj się z Nami
+          Dziś: Zespół doświadczonych elektryków gotów do obsługi różnorodnych
+          projektów Zapewniamy kompleksowe usługi elektryczne, zgodne z
+          najnowszymi standardami i przepisami
+        </p>
+        <div class="services-icon">
+          <div class="services-icon__rect"></div>
+          <HeatIcon class="transform" />
+        </div>
+        <h2 class="title title--h2 title--bold">Pompy ciepła</h2>
+        <!-- eslint-disable-next-line -->
+        <p class="text">
+          Dlaczego Wybrać Nasze Usługi: Doświadczeni elektrycy z wiedzą w
+          zakresie różnorodnych instalacji elektrycznych Specjalizacja w
+          instalacjach fotowoltaicznych, dostarczając energię ze źródeł
+          odnawialnych Kompleksowe podejście do montażu pomp ciepła, z
+          uwzględnieniem integracji z systemem grzewczym Skontaktuj się z Nami
+          Dziś: Zespół doświadczonych elektryków gotów do obsługi różnorodnych
+          projektów Zapewniamy kompleksowe usługi elektryczne, zgodne z
+          najnowszymi standardami i przepisami
+        </p>
+        <div class="services-icon">
+          <div class="services-icon__rect"></div>
+          <AvgPaceIcon class="transform" />
+        </div>
+        <h2 class="title title--h2 title--bold">Pomiary</h2>
+        <!-- eslint-disable-next-line -->
+        <p class="text">
+          Dlaczego Wybrać Nasze Usługi: Doświadczeni elektrycy z wiedzą w
+          zakresie różnorodnych instalacji elektrycznych Specjalizacja w
+          instalacjach fotowoltaicznych, dostarczając energię ze źródeł
+          odnawialnych Kompleksowe podejście do montażu pomp ciepła, z
+          uwzględnieniem integracji z systemem grzewczym Skontaktuj się z Nami
+          Dziś: Zespół doświadczonych elektryków gotów do obsługi różnorodnych
+          projektów Zapewniamy kompleksowe usługi elektryczne, zgodne z
+          najnowszymi standardami i przepisami
+        </p>
+        <div class="services-icon">
+          <div class="services-icon__rect"></div>
+          <InstantMixIcon class="transform" />
+        </div>
+        <h2 class="title title--h2 title--bold">Rozdzielnice</h2>
+        <!-- eslint-disable-next-line -->
+        <p class="text">
+          Dlaczego Wybrać Nasze Usługi: Doświadczeni elektrycy z wiedzą w
+          zakresie różnorodnych instalacji elektrycznych Specjalizacja w
+          instalacjach fotowoltaicznych, dostarczając energię ze źródeł
+          odnawialnych Kompleksowe podejście do montażu pomp ciepła, z
+          uwzględnieniem integracji z systemem grzewczym Skontaktuj się z Nami
+          Dziś: Zespół doświadczonych elektryków gotów do obsługi różnorodnych
+          projektów Zapewniamy kompleksowe usługi elektryczne, zgodne z
+          najnowszymi standardami i przepisami
+        </p>
+      </div>
+      <div class="">
+        <!-- eslint-disable-next-line -->
+        <h2 class="title title--h2 title--bold">Dlaczego my?</h2>
+        <div class="underline underline--mb"></div>
+        <!-- eslint-disable-next-line -->
+        <p class="text">
+          Dlaczego Wybrać Nasze Usługi: Doświadczeni elektrycy z wiedzą w
+          zakresie różnorodnych instalacji elektrycznych Specjalizacja w
+          instalacjach fotowoltaicznych, dostarczając energię ze źródeł
+          odnawialnych Kompleksowe podejście do montażu pomp ciepła, z
+          uwzględnieniem integracji z systemem grzewczym Skontaktuj się z Nami
+          Dziś: Zespół doświadczonych elektryków gotów do obsługi różnorodnych
+          projektów Zapewniamy kompleksowe usługi elektryczne, zgodne z
+          najnowszymi standardami i przepisami
+        </p>
+      </div>
+      <div class="">
+        <!-- eslint-disable-next-line -->
+        <h2 class="title title--h2 title--bold">Skontaktuj się z nami!</h2>
+        <div class="underline underline--mb"></div>
+        <p class="text">
+          <!-- eslint-disable-next-line -->
+          Jesteśmy do Twojej dyspozycji od poniedziałku do piątku w godzinach
+          8.00-16.00
+        </p>
+        <CustomButton class="bg-granat">Skontaktuj się</CustomButton>
+      </div>
+    </div>
+    <FooterElektro />
   </div>
 </template>
+<style lang="scss" scoped>
+.services {
+  padding-top: 10px;
+  background: #fff;
+}
+
+.image-wrapper {
+  display: grid;
+  grid-template-columns: 0.05fr 0.95fr;
+  margin-bottom: 20px;
+}
+.rectangle {
+  position: relative;
+  &--yellow {
+    background-color: #f9cb53;
+  }
+}
+.services-icon {
+  display: flex;
+  align-items: center;
+  &__rect {
+    width: 50%;
+    height: 35px;
+    flex-shrink: 0;
+    background-color: #f9cb53;
+  }
+  .transform {
+    transform: translateX(-26px);
+  }
+}
+</style>
