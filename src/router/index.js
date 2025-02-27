90c6b2d58e6be5be3971d278f8fece58816b49dc
ErykFryderyk <grzeniu098@mgail.com>
2025-02-27 22:20:50 +0100

    github pages test

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return { top: 0 };
